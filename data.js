292741527e861c7b244a5291b0b6fc2d33ff0b3c
𝙆𝙖𝙧𝙢𝙖 <126420979+karmameteor@users.noreply.github.com>
2026-02-08 14:36:01 +0800

    Update data.js

--- data.js
+++ data.js
@@ -7,21 +7,21 @@ window.ITEMS = [
   {
     name:"Master Ring",
     price:8,
-    img:"https://maplestory.io/api/gms/latest/item/1112405/icon"
+    img:"https://deanmaple97.github.io/tms-lib/images/Character/Ring/01112792.png"
   },
   {
     name:"Chaos Horntail Necklace",
     price:60,
-    img:"https://maplestory.io/api/gms/latest/item/1122000/icon"
+    img:"https://deanmaple97.github.io/tms-lib/images/Character/Accessory/01122076.png"
   },
   {
     name:"VIP Belt",
     price:null,
-    img:"https://maplestory.io/api/gms/latest/item/1132010/icon"
+    img:"https://deanmaple97.github.io/tms-lib/images/Character/Accessory/01132040.png"
   },
   {
     name:"Sword Earring",
     price:10,
-    img:"https://maplestory.io/api/gms/latest/item/1032024/icon"
+    img:"https://deanmaple97.github.io/tms-lib/images/Character/Accessory/01032030.png"
   }
 ];

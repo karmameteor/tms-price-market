a2c8225f3858b5629df2041a6cdf7dc5a3ae49d7
𝙆𝙖𝙧𝙢𝙖 <126420979+karmameteor@users.noreply.github.com>
2026-02-08 18:18:45 +0800

    Add top nav, sidebar filters, and footer

--- data.js
+++ data.js
@@ -1,26 +1,41 @@
 window.ITEMS = [
   {
     name:"Chaos Zakum Helmet",
+    meta:"Equip LV 140 / Boss / Helmet",
+    category:"Equips",
+    enhanceable:true,
     price:20,
     img:"https://deanmaple97.github.io/tms-lib/images/Character/Cap/01003112.png"
   },
   {
     name:"Master Ring",
+    meta:"Equip LV 130 / Ring / Accessory",
+    category:"Equips",
+    enhanceable:true,
     price:8,
     img:"https://deanmaple97.github.io/tms-lib/images/Character/Ring/01112792.png"
   },
   {
     name:"Chaos Horntail Necklace",
+    meta:"Equip LV 120 / Boss / Accessory",
+    category:"Equips",
+    enhanceable:true,
     price:60,
     img:"https://deanmaple97.github.io/tms-lib/images/Character/Accessory/01122076.png"
   },
   {
     name:"VIP Belt",
+    meta:"Equip LV 120 / Belt / Accessory",
+    category:"Equips",
+    enhanceable:false,
     price:null,
     img:"https://deanmaple97.github.io/tms-lib/images/Character/Accessory/01132040.png"
   },
   {
     name:"Sword Earring",
+    meta:"Equip LV 110 / Earring / Accessory",
+    category:"Equips",
+    enhanceable:true,
     price:10,
     img:"https://deanmaple97.github.io/tms-lib/images/Character/Accessory/01032030.png"
   }

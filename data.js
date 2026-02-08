7f2acec15b0657b8bd194f771a59821e43a6c4ed
𝙆𝙖𝙧𝙢𝙖 <126420979+karmameteor@users.noreply.github.com>
2026-02-08 14:26:29 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,32 +1,32 @@
 window.ITEMS = [
   {
-    name:"Chaos Zakum Helmet",
-    issued:2800,
-    price:20,
-    img:"https://deanmaple97.github.io/tms-lib/images/Character/Cap/01003112.png"
+    name: "Chaos Zakum Helmet",
+    issued: 2800,
+    price: 20,
+    img: "https://picsum.photos/seed/czak/600/400"
   },
   {
-    name:"Master Ring",
-    issued:3500,
-    price:8,
-    img:"https://deanmaple97.github.io/tms-lib/images/Character/Ring/01112792.png"
+    name: "Master Ring",
+    issued: 3500,
+    price: 8,
+    img: "https://picsum.photos/seed/master/600/400"
   },
   {
-    name:"Chaos Horntail Necklace",
-    issued:4900,
-    price:60,
-    img:"https://deanmaple97.github.io/tms-lib/images/Character/Accessory/01122076.png"
+    name: "Timeless Gear",
+    issued: 4900,
+    price: null,
+    img: "https://picsum.photos/seed/timeless/600/400"
   },
   {
-    name:"VIP Belt",
-    issued:3029,
-    price:null,
-    img:"https://deanmaple97.github.io/tms-lib/images/Character/Accessory/01132040.png"
+    name: "VIP Belt",
+    issued: 3029,
+    price: null,
+    img: "https://picsum.photos/seed/vip/600/400"
   },
   {
-    name:"Sword Earring",
-    issued:2589,
-    price:10,
-    img:"https://deanmaple97.github.io/tms-lib/images/Character/Accessory/01032030.png"
+    name: "Sword Earring",
+    issued: 2589,
+    price: 10,
+    img: "https://picsum.photos/seed/sword/600/400"
   }
 ];

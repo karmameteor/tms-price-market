a33364948a35701084dd60fde9b5eea8eb23b83f
𝙆𝙖𝙧𝙢𝙖 <126420979+karmameteor@users.noreply.github.com>
2026-02-08 14:31:24 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,32 +1,27 @@
 window.ITEMS = [
   {
-    name: "Chaos Zakum Helmet",
-    issued: 2800,
-    price: 20,
-    img: "https://picsum.photos/seed/czak/600/400"
+    name:"Chaos Zakum Helmet",
+    price:20,
+    img:"https://maplestory.io/api/gms/latest/item/1002357/icon"
   },
   {
-    name: "Master Ring",
-    issued: 3500,
-    price: 8,
-    img: "https://picsum.photos/seed/master/600/400"
+    name:"Master Ring",
+    price:8,
+    img:"https://maplestory.io/api/gms/latest/item/1112405/icon"
   },
   {
-    name: "Timeless Gear",
-    issued: 4900,
-    price: null,
-    img: "https://picsum.photos/seed/timeless/600/400"
+    name:"Chaos Horntail Necklace",
+    price:60,
+    img:"https://maplestory.io/api/gms/latest/item/1122000/icon"
   },
   {
-    name: "VIP Belt",
-    issued: 3029,
-    price: null,
-    img: "https://picsum.photos/seed/vip/600/400"
+    name:"VIP Belt",
+    price:null,
+    img:"https://maplestory.io/api/gms/latest/item/1132010/icon"
   },
   {
-    name: "Sword Earring",
-    issued: 2589,
-    price: 10,
-    img: "https://picsum.photos/seed/sword/600/400"
+    name:"Sword Earring",
+    price:10,
+    img:"https://maplestory.io/api/gms/latest/item/1032024/icon"
   }
 ];
